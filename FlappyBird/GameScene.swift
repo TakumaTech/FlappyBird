99971199a473870019b8b407ad42923cfa9ac5a6
takuma.beppu <takuma.beppu@gmail.com>
2020-02-23 14:09:18 +0900

    lesson7 5-2から

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,26 @@
+    //
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by t on 2020/02/23.
+//  Copyright © 2020 takuma.beppu. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
